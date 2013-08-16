bf0b9d5284ac97b0604d859b107b35de30efea3f
Gustaf Mäkinen Nordquist <gustafmakinennordquist@gmail.com>
2013-08-16 15:41:17 +0200

    Added functions for 'all callback'

--- chain.js
+++ chain.js
@@ -1,5 +1,9 @@
 var chain=function(){
 
+	var callback = (arguments.length%2==0)?null:
+		Array.prototype.splice.call(arguments, arguments.length-1)[0];
+
+		alert(callback)
 	var serie=[];
 	var c=0;
 
@@ -24,14 +28,20 @@ var chain=function(){
 
 
 	//Chain callbacks to next initiator
-	for(var i=0;i<(serie.length-1);i++){
+	for(var i=0;i<(serie.length);i++){
 		var cb=serie[i].cb;
-		serie[i].cb=(function(i, cb){
-			return function(){
-				cb.call();
+		serie[i].cb=(i<(serie.length-1))?(function(i, cb){
+			return function(arguments){
+				cb.call(arguments);
 				serie[i+1].fn.call(serie[i+1].fn,serie[i+1].cb);
 			}
-		})(i, cb);
+		})(i, cb)
+		:(function(i, cb){
+			return function(arguments){
+				cb.call(arguments);
+				callback.call();
+			}
+		})(callback,cb);
 	}
 	serie[0].fn.call(serie[0].fn,serie[0].cb);
 }
